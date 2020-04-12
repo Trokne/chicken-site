153d95dc323664bb942f70dcf0dc850baac6540b
Mikhail Vshivkov <vshivkov.ma@gmail.com>
2020-04-12 20:56:04 +0500

    Updates

---
+++ precache-manifest.5640b6d4418cb256894b8778d2001fdc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ef3908df6a9e563ec4349b7c450837",
+    "url": "/chicken-site/index.html"
+  },
+  {
+    "revision": "30c03119b792b6bcddfd",
+    "url": "/chicken-site/static/css/main.ac49484a.chunk.css"
+  },
+  {
+    "revision": "d7779597e62aadeeb470",
+    "url": "/chicken-site/static/js/2.d7663317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chicken-site/static/js/2.d7663317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c03119b792b6bcddfd",
+    "url": "/chicken-site/static/js/main.bc294a3c.chunk.js"
+  },
+  {
+    "revision": "a38b87639b9b04ce9ba7",
+    "url": "/chicken-site/static/js/runtime-main.60a11865.js"
+  },
+  {
+    "revision": "b870717bbcdc615c4eda4031b04122d6",
+    "url": "/chicken-site/static/media/chickens.b870717b.jpg"
+  },
+  {
+    "revision": "473b9e3ebdd7e4a177c1d6144b90b4c6",
+    "url": "/chicken-site/static/media/telegram.473b9e3e.png"
+  },
+  {
+    "revision": "5b17f89aaecefa21138879eff986f3bb",
+    "url": "/chicken-site/static/media/viber.5b17f89a.png"
+  },
+  {
+    "revision": "7084bfaf938e7371a117c00be6dcfcfa",
+    "url": "/chicken-site/static/media/whatsapp.7084bfaf.png"
+  }
+]);
