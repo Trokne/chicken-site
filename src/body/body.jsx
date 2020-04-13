132fbed8698a9015346e4b8d6704ce109aeb8f17
Mikhail Vshivkov <vshivkov.ma@gmail.com>
2020-04-13 23:00:33 +0500

    Исправление разметки

--- src/body/body.jsx
+++ src/body/body.jsx
@@ -10,25 +10,27 @@ class Body extends React.Component {
       <div className="buy-body">
         <div className="bg-image" />
         <div className="bg-text">
-          <div className="description">
-            Продажа куриц с доставкой по Перми и Пермскому краю <br /> Работаем
-            на заказ
-          </div>
-          <div className="button-container">
-            <a href="tel:79024785293" className="phone-number-button">
-              Позвонить на +7 902 478 52 93
-            </a>
-          </div>
-          <div className="social-container">
-            <a href="tel:79024785293" className="social">
-              <img src={whatsapp} alt="" className="social-img" />
-            </a>
-            <a href="tel:79024785293" className="social">
-              <img src={telegram} alt="" className="social-img" />
-            </a>
-            <a href="tel:79024785293" className="social">
-              <img src={viber} alt="" className="social-img" />
-            </a>
+          <div className="bg-text-container">
+            <div className="description">
+              Продажа куриц с доставкой по Перми и Пермскому краю <br />{" "}
+              Работаем на заказ
+            </div>
+            <div className="button-container">
+              <a href="tel:79024785293" className="phone-number-button">
+                Позвонить на +7 902 478 52 93
+              </a>
+            </div>
+            <div className="social-container">
+              <a href="tel:79024785293" className="social">
+                <img src={whatsapp} alt="" className="social-img" />
+              </a>
+              <a href="tel:79024785293" className="social">
+                <img src={telegram} alt="" className="social-img" />
+              </a>
+              <a href="tel:79024785293" className="social">
+                <img src={viber} alt="" className="social-img" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
